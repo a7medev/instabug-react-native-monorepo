e0dfe5374dc2d8d5f1fa678e689a7737e44780b9
Ahmed Mahmoud <a7med.mahmoud2004@gmail.com>
2023-05-10 18:27:48 +0300

    feat: integrate Instabug

--- packages/mobile/App.tsx
+++ packages/mobile/App.tsx
@@ -1,17 +1,20 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 import {Button} from '@instabug-react-native-monorepo/shared';
+import Instabug, {InvocationEvent} from 'instabug-reactnative';
 
 const App: React.FC = () => {
+  useEffect(() => {
+    Instabug.init({
+      token: 'deb1910a7342814af4e4c9210c786f35',
+      invocationEvents: [InvocationEvent.floatingButton],
+    });
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Hello from Instabug{'\n'}ft. monorepo!</Text>
-      <Button
-        onPress={() => {
-          // TODO: Add Instabug
-        }}>
-        Show Instabug
-      </Button>
+      <Button onPress={Instabug.show}>Show Instabug</Button>
     </View>
   );
 };
